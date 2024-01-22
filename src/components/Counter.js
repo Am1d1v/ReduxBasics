06d78e3223ed2680186a5a66626dfaefe9c784c2
Am1d1v <dimadiman1adima@gmail.com>
2024-01-22 16:41:55 +0300

    React Redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,18 @@
 import classes from "./Counter.module.css";
+import {useSelector } from "react-redux";
 
 const Counter = () => {
+
+  const counter =  useSelector((state) => {
+    return state.counter;
+  });
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Счётчик</h1>
-      <div className={classes.value}>0</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Спрятать / Показать</button>
     </main>
   );
